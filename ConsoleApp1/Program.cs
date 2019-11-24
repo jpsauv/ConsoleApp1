a1455d49dd92b6beb4a86da11b92f735e05b3864
James Sauve <sauvejs@mail.uc.edu>
2019-11-23 23:30:43 -0500

    cleared Programcs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -10,8 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string name = "James";
-            Console.WriteLine($"Hello {name}!!!");
+            
         }
     }
 }
