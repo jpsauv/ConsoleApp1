ec81a6bb5c3d301f837c28fa573aa6aac92ab39c
James Sauve <sauvejs@mail.uc.edu>
2019-12-21 15:06:03 -0500

    added read file back

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            string[] lines = new string[0];
+
+            try
+            {
+                lines = File.ReadAllLines(@"C:\Users\James Sauve\Desktop\Creed.txt");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The file was not found or there was another error.");
+            }
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }
